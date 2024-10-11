d3a93fa81fe8d6b6ae9da90d51c1565455bfe99d
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-10-11 23:10:43 +0300

    This means war

---
+++ src/main/java/tech/majaliwa/thismeanswar/ThisMeansWarApplication.java
@@ -0,0 +1,13 @@
+package tech.majaliwa.thismeanswar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisMeansWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThisMeansWarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/majaliwa/thismeanswar/ThisMeansWarApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.majaliwa.thismeanswar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisMeansWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
